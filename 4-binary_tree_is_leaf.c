1e8964b94b581ed6487705169a3f638c76e3bc59
Obi Chukwu <kennetho.onu@gmail.com>
2024-03-28 05:54:48 +0800

    checks if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,8 +7,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL)
 		return (0);
 
-	return (1);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	return (0);
 }
