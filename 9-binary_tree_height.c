1b9c0a5135246ae62a5f13b2c39a0619ad3aa063
Obi Chukwu <kennetho.onu@gmail.com>
2024-03-28 09:20:45 +0800

    measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
+	if (left_height >= right_height)
 		return (left_height + 1);
 	else
 		return (right_height + 1);
